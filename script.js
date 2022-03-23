7985d234f2693c509f2d11df83848716551b0aa9
sob16 <imamsobirin@mail.ugm.ac.id>
2022-03-23 17:35:49 +0700

    add responsive

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const about = document.querySelector(".about-me");
+const menuBar = document.querySelector(".menu-bar"),
+  menuList = document.querySelector(".navbar-list");
 
+menuBar.addEventListener("click", (e) => {
+  if (menuList.style.display == "none") {
+    menuList.style.display = "block";
+  } else {
+    menuList.style.display = "none";
+  }
+});
